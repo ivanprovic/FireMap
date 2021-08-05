b8894fe95a71ae3959777dc02fec859001523042
Ivan Provic <ivan.pik82@gmail.com>
2021-08-05 19:22:43 +0200

    final stage

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import spinner from "./spinner.gif";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <img src={spinner} alt="Loading" />
+      <h1>Loading Data</h1>
+    </div>
+  );
+};
+
+export default Loader;
